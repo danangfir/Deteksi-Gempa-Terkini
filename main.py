ac8c2e43e20a7cb5bfcae38fec1bccfb396b25d6
danangfir <danangfirmino26@gmail.com>
2023-08-18 07:29:54 +0700

    menghapus

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ Aplikasi Deteksi Gempa Terkini
 MODULARISASI DENGAN FUNCTION
 MODULARISASI DENGAN PACKAGE
 """
-import gempa_terkini
+from gempa_terkini import ekstrasi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('aplikasi utama')
     print('\n')
-    result = gempa_terkini.ekstrasi_data()
-    gempa_terkini.tampilkan_data(result)
+    result = ekstrasi_data()
+    tampilkan_data(result)
