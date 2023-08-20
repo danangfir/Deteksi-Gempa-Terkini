94395f7627929896a400c5d246034adf9dbb6c1c
danangfir <danangfirmino26@gmail.com>
2023-08-20 22:44:45 +0700

    try and except

--- data_gempa/__init__.py
+++ data_gempa/__init__.py
@@ -1,4 +1,6 @@
-import bs4
+import requests
+from bs4 import BeautifulSoup
+
 
 def ekstrasi_data():
     """
@@ -12,20 +14,32 @@ def ekstrasi_data():
     :return:
     """
 
-    soup = bs4.BeautifulSoup("<p>Some<b>bad<i>HTML")
+    try:
+        content = requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        print(content.text)
+        # soup = BeautifulSoup(content)
+        # print(soup.prettify())
 
-    hasil = dict()
-    hasil['tanggal'] = '24 agustus 2023'
-    hasil['waktu'] = '16:29:17 WIB'
-    hasil['magnitudo'] = '2.6 skala richter'
-    hasil['lokasi'] = {'ls': 6.82, 'bt': 107.13}
-    hasil['pusat'] = 'Pusat gempa berada di darat 1 km barat laut Kab. Cianjur'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II Cugenang, II Kota Cianjur, II Karangtengah, II Warungkondang'
+        hasil = dict()
+        hasil['tanggal'] = '24 agustus 2023'
+        hasil['waktu'] = '16:29:17 WIB'
+        hasil['magnitudo'] = '2.6 skala richter'
+        hasil['lokasi'] = {'ls': 6.82, 'bt': 107.13}
+        hasil['pusat'] = 'Pusat gempa berada di darat 1 km barat laut Kab. Cianjur'
+        hasil['dirasakan'] = 'Dirasakan (Skala MMI): II Cugenang, II Kota Cianjur, II Karangtengah, II Warungkondang'
 
-    return hasil
+        return hasil
+    else:
+        return None
 
 
 def tampilkan_data(result):
+    if result is None:
+        print('Tidak bisa Menemukan data gempa terkini')
+        return
     print('Gempa Terakhir Berdasarkan BMKG')
     print(f"Tanggal: {result['tanggal']}")
     print(f"Waktu: {result['waktu']}")
@@ -33,5 +47,3 @@ def tampilkan_data(result):
     print(f"lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
     print(f"pusat: {result['pusat']}")
     print(f"dirasakan: {result['dirasakan']}")
-
-
