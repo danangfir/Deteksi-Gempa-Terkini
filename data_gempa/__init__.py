ca20dc8570b847bcb01789c46fd9b69dd282f957
danangfir <danangfirmino26@gmail.com>
2023-08-22 08:43:11 +0700

    final

--- data_gempa/__init__.py
+++ data_gempa/__init__.py
@@ -19,17 +19,46 @@ def ekstrasi_data():
     except Exception:
         return None
     if content.status_code == 200:
-        print(content.text)
-        # soup = BeautifulSoup(content)
-        # print(soup.prettify())
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        result = soup.find('span', {'class': 'waktu'})
+        result = result.text.split(', ')
+        tanggal = result[0]
+        waktu = result[1]
+
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        i = 0
+        magnitudo = None
+        kedalaman = None
+        ls = None
+        bt = None
+        lokasi = None
+        dirasakan = None
+        for res in result:
+            print(i, res)
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+            i = i + 1
 
         hasil = dict()
-        hasil['tanggal'] = '24 agustus 2023'
-        hasil['waktu'] = '16:29:17 WIB'
-        hasil['magnitudo'] = '2.6 skala richter'
-        hasil['lokasi'] = {'ls': 6.82, 'bt': 107.13}
-        hasil['pusat'] = 'Pusat gempa berada di darat 1 km barat laut Kab. Cianjur'
-        hasil['dirasakan'] = 'Dirasakan (Skala MMI): II Cugenang, II Kota Cianjur, II Karangtengah, II Warungkondang'
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+        hasil['koordinat'] = {'ls': ls, 'bt': bt}
+        hasil['lokasi'] = lokasi
+        hasil['dirasakan'] = dirasakan
 
         return hasil
     else:
@@ -44,6 +73,7 @@ def tampilkan_data(result):
     print(f"Tanggal: {result['tanggal']}")
     print(f"Waktu: {result['waktu']}")
     print(f"Magnitudo: {result['magnitudo']}")
-    print(f"lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"pusat: {result['pusat']}")
+    print(f"kedalaman: {result['kedalaman']}")
+    print(f"koordinat: LS={result['koordinat']['ls']}, BT={result['koordinat']['bt']}")
+    print(f"lokasi: {result['lokasi']}")
     print(f"dirasakan: {result['dirasakan']}")
--- main.py
+++ main.py
@@ -6,8 +6,7 @@ MODULARISASI DENGAN FUNCTION
 import data_gempa
 
 if __name__ == '__main__':
-    print('aplikasi utama')
-    print('\n')
+    print('APLIKASI UTAMA>>>>>>>>>>>//////////<<<<<<<<<<')
     result = data_gempa.ekstrasi_data()
     data_gempa.tampilkan_data(result)
 
